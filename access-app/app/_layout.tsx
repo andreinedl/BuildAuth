f143989d6e8299b85eae9aa5b060388a8d4376b1
Andrei Nedelcu <andreinedelcu48@gmail.com>
2024-10-12 17:38:20 +0000

    Initial commit - Start Android App (expo)

---
+++ access-app/app/_layout.tsx
@@ -0,0 +1,28 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import { useColorScheme } from 'react-native'
+import { TamaguiProvider } from 'tamagui'
+import { useFonts } from 'expo-font'
+
+import { tamaguiConfig } from '../tamagui.config'
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme()
+
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Regular.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  })
+
+  if (!loaded) {
+    return null
+  }
+
+  return (
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack />
+      </ThemeProvider>
+    </TamaguiProvider>
+  )
+}
